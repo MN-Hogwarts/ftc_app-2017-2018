0ac2c2b563768166a94a28a7e94fbe12ee02993b
Mrinali <mrinalinakhre.7@gmail.com>
2017-09-29 00:39:12 -0400

    More changes to MecanumMotors, which is to be used with other hardware classes Adding updated TRC library files to Git

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumMotors.java
@@ -48,10 +48,6 @@ public class MecanumMotors
     public DcMotor  leftRearDrive   = null;
     public DcMotor  rightRearDrive  = null;
 
-    public static final double MID_SERVO       =  0.5 ;
-    public static final double ARM_UP_POWER    =  0.45 ;
-    public static final double ARM_DOWN_POWER  = -0.45 ;
-
     /* local OpMode members. */
     HardwareMap hwMap           =  null;
     private ElapsedTime period  = new ElapsedTime();
