0a002c210797b8f07a3db559639ab355f937e920
spmega <spmega4567@gmail.com>
2017-11-17 23:30:12 -0500

    Finished modifying teleop to be cleaner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumRobotCentricTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumRobotCentricTeleop.java
@@ -25,32 +25,9 @@ import trclib.TrcTaskMgr;
  * Created by spmeg on 4/15/2017.
  */
 @TeleOp(name = "MecanumRobotCentricTeleop", group = "teleop")
-public class MecanumRobotCentricTeleop extends OpMode implements SWGamePad.ButtonHandler, Runnable{
-    /*
-    private final double LOADED_POSITION_TRIGGER = 0.557;
-    private final double SHOOT_POSITION_TRIGGER = 0.428;
-    private final double START_POSITION_ANGULAR = 0.3;
-    private final double START_POSITION_TURRET = 0.50;
-    private final double CLOCKWISE_POSITION_TURRET = 0.40;
-    private final double COUNTERCLOCKWISE_POSITION_TURRET = 0.60;
-    private final double SHOOTER_POWER = 1.0;
-    private final double REVERSE_SHOOTER_POWER = -0.5;
-    private final double SERVO_INCREMENT = 0.0025;
-
-    private Servo triggerServo, angularServo, turretServo;
-    */
+public class MecanumRobotCentricTeleop extends OpMode{
 
     private boolean OP_MODE_IS_ACTIVE = true;
-    /*
-    private boolean SHOOTER_MOTORS_ACTIVE = false;
-    private boolean SHOOTER_MOTORS_REVERSE = false;
-    private boolean SHOOTER_MOTORS_IS_READY = false;
-
-    private DcMotor leftShooter, rightShooter;
-
-    boolean               inputPin;             // Input State
-    DigitalChannel digIn;                // Device Object
-    */
 
     private FtcDcMotor leftFrontMotor;
     private FtcDcMotor leftRearMotor;
@@ -61,47 +38,23 @@ public class MecanumRobotCentricTeleop extends OpMode implements SWGamePad.Butto
     private SwDriveBase driveBase = null;
     private SWIMUGyro gyro = null;
     private SWGamePad gamepad;
-    private boolean fixedOnTarget = false;
-    private HalDashboard dashboard = null;
-    private final int LABEL_WIDTH = 200;
+    //private HalDashboard dashboard = null;
     private boolean setYInverted = true;
-    private boolean setXInverted = false;
-    private TrcTaskMgr taskMgr = new TrcTaskMgr();
-    private HashMap<Integer, ElapsedTime> toggleTimeTracker = new HashMap<>();
     private TrcServo jewelServo = null;
     private Servo wristServo;
     private Servo leftPickupServo, rightPickupServo;
     private Servo relicServo;
     private DigitalChannel touchSensor ;
-    private PickupHardware pickupHw = new PickupHardware();
-    static double wristServoValue = 0.04;
     private static double gyroKp = 0.;
     private static double gyroScale = 0.;
     private boolean turtleMode = false;
     private double magnitude = 0;
-    private boolean adjustGyroScale = true;
+    //private boolean adjustGyroScale = true;
     private double relicServPos = 0.7;
-
-    /*
-    @Override
-    public void init_loop() {
-        super.init_loop();
-        taskMgr.executeTaskType(TrcTaskMgr.TaskType.PREPERIODIC_TASK, TrcRobot.RunMode.TELEOP_MODE);
-        taskMgr.executeTaskType(TrcTaskMgr.TaskType.PRECONTINUOUS_TASK, TrcRobot.RunMode.TELEOP_MODE);
-    }
-
-
-    @Override
-    public void start() {
-        super.start();
-        taskMgr.executeTaskType(TrcTaskMgr.TaskType.START_TASK, TrcRobot.RunMode.TELEOP_MODE);
-    }
-    */
 
     @Override
     public void stop() {
         super.stop();
-        taskMgr.executeTaskType(TrcTaskMgr.TaskType.STOP_TASK, TrcRobot.RunMode.TELEOP_MODE);
         OP_MODE_IS_ACTIVE = false;
     }
 
@@ -109,7 +62,7 @@ public class MecanumRobotCentricTeleop extends OpMode implements SWGamePad.Butto
 
     @Override
     public void init() {
-        dashboard = HalDashboard.createInstance(this.telemetry);
+        //dashboard = HalDashboard.createInstance(this.telemetry);
         jewelServo = new FtcServo(this.hardwareMap, "jewelArm");
         jewelServo.setPosition(0.9);
 
@@ -151,28 +104,7 @@ public class MecanumRobotCentricTeleop extends OpMode implements SWGamePad.Butto
         gamepad.enableDebug(true);
 
         driveBase.enableGyroAssist(gyroScale, gyroKp);
-        /*
-        triggerServo = hardwareMap.servo.get("triggerServo");
-        angularServo = hardwareMap.servo.get("pixyyaxis");
-        turretServo = hardwareMap.servo.get("pixyyrotation");
 
-        leftShooter = hardwareMap.dcMotor.get("shooterLeft");
-        rightShooter = hardwareMap.dcMotor.get("shooterRight");
-
-        leftShooter.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        leftShooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightShooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        digIn  = hardwareMap.get(DigitalChannel.class, "digin");     //  Use generic form of device mapping
-        digIn.setMode(DigitalChannelController.Mode.INPUT);          // Set the direction of each channel
-
-        triggerServo.setPosition(LOADED_POSITION_TRIGGER);
-        angularServo.setPosition(START_POSITION_ANGULAR);
-        turretServo.setPosition(START_POSITION_TURRET);
-        */
-
-        new Thread(this).start();
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -190,6 +122,13 @@ public class MecanumRobotCentricTeleop extends OpMode implements SWGamePad.Butto
 
                     //driveBase.mecanumDrive_XPolarFieldCentric(magnitude, direction, rotation);
                     driveBase.mecanumDrive_XPolar(magnitude, direction, rotation);
+                    jewelServo.setPosition(0.9);
+
+                    if(gamepad1.a){
+                        turtleMode = true;
+                    } else if(gamepad1.b){
+                        turtleMode = false;
+                    }
                 }
             }
         }).start();
@@ -267,289 +206,9 @@ public class MecanumRobotCentricTeleop extends OpMode implements SWGamePad.Butto
 
     @Override
     public void loop() {
-        taskMgr.executeTaskType(TrcTaskMgr.TaskType.PREPERIODIC_TASK, TrcRobot.RunMode.TELEOP_MODE);
-        taskMgr.executeTaskType(TrcTaskMgr.TaskType.PRECONTINUOUS_TASK, TrcRobot.RunMode.TELEOP_MODE);
-        taskMgr.executeTaskType(TrcTaskMgr.TaskType.PREPERIODIC_TASK, TrcRobot.RunMode.AUTO_MODE);
-        taskMgr.executeTaskType(TrcTaskMgr.TaskType.PRECONTINUOUS_TASK, TrcRobot.RunMode.AUTO_MODE);
-
-        /*
-        gamepad.setYInverted(setYInverted);
-        double rotation = gamepad.getRightStickX()*-1;
-        double magnitude = Range.clip(gamepad.getLeftStickMagnitude(), 0, 1);
-        if(turtleMode)
-            magnitude = magnitude/2;
-
-        double direction = gamepad.getLeftStickDirectionDegrees(true);
-
-        if(gamepad.getLeftStickX() == 0 && gamepad.getLeftStickY() == 0)
-            magnitude = 0;
-
-        //driveBase.mecanumDrive_XPolarFieldCentric(magnitude, direction, rotation);
-        driveBase.mecanumDrive_XPolar(magnitude, direction, rotation);
-        */
-
-        if(gamepad1.x){
-            adjustGyroScale = true;
-        } else if (gamepad1.y){
-            adjustGyroScale = false;
-        }
-
-        if (adjustGyroScale){
-            if(gamepad1.dpad_up){
-                gyroScale = gyroScale + 0.05;
-                gyroScale = Range.clip(gyroScale, 0, 1);
-            } else if(gamepad1.dpad_down){
-                gyroScale = gyroScale - 0.05;
-                gyroScale = Range.clip(gyroScale, 0, 1);
-            }
-        } else {
-            if(gamepad1.dpad_up){
-                gyroKp = gyroKp + 0.05;
-                gyroKp = Range.clip(gyroKp, 0, 1);
-            } else if(gamepad1.dpad_down){
-                gyroKp = gyroKp - 0.05;
-                gyroKp = Range.clip(gyroKp, 0, 1);
-            }
-        }
-
-        //driveBase.enableGyroAssist(gyroScale, gyroKp);
-
-        if(gamepad1.a){
-            turtleMode = true;
-        } else if(gamepad1.b){
-            turtleMode = false;
-        }
-
-        if(gamepad1.left_bumper){
-            //gyroScale += 0.05;
-            jewelServo.setPosition(0.9);
-        }
-
-
-        /*
-        if (gamepad2.b) {
-            leftPickupServo.setPosition(-1.0);
-            rightPickupServo.setPosition(1.0);
-        } //If touch sensor is pressed, stop wheels. If 'A' is pressed, run wheels. If neither is pressed, stop wheels
-        else if (!touchSensor.getState()) {
-            leftPickupServo.setPosition(0.53);
-            rightPickupServo.setPosition(0.5);
-        } //Turn inward
-        else if (gamepad2.a) {
-            leftPickupServo.setPosition(1.0);
-            rightPickupServo.setPosition(-1.0);
-        } //Stop wheels
-        else if (gamepad2.left_bumper) {
-            leftPickupServo.setPosition(1.0);
-            rightPickupServo.setPosition(0.5);
-        }
-        else if (gamepad2.right_bumper) {
-            rightPickupServo.setPosition(-1.0);
-            leftPickupServo.setPosition(0.53);
-        }
-        else {
-            leftPickupServo.setPosition(0.53);
-            rightPickupServo.setPosition(0.5);
-        }
-
-        if (gamepad2.y){
-            wristServo.setPosition(1);
-            //wristServoValue = wristServoValue + 0.05;
-        } else if (gamepad2.x) {
-            //wristServoValue = Range.clip(wristServoValue, -1.0, 1.0);
-            //wristServo.setPosition(wristServoValue);
-            wristServo.setPosition(0);
-        } else {
-            wristServo.setPosition(0.5);
-        }
-        */
-
-        /*
-        if(gamepad2.dpad_up){
-            armMotorSpeedLimiter = armMotorSpeedLimiter + 0.01;
-        } else if(gamepad2.dpad_down){
-            armMotorSpeedLimiter = armMotorSpeedLimiter - 0.01;
-        }
-
-        armMotorSpeedLimiter = Range.clip(armMotorSpeedLimiter, 0, 1);
-
-        armMotor.setPower(gamepad2.left_stick_y*armMotorSpeedLimiter);
-
-        if (gamepad1.dpad_right){
-            //relicServo.setPosition(0.3);
-            relicServPos = 0.3;
-        } else if (gamepad1.dpad_left){
-            //relicServo.setPosition(0.7);
-            relicServPos = 0.7;
-        }
-
-        relicServo.setPosition(relicServPos);
-        */
-
-        /*
-        if(SHOOTER_MOTORS_REVERSE){
-            leftShooter.setPower(REVERSE_SHOOTER_POWER);
-            rightShooter.setPower(REVERSE_SHOOTER_POWER);
-        } else if (SHOOTER_MOTORS_ACTIVE){
-            leftShooter.setPower(SHOOTER_POWER);
-            rightShooter.setPower(SHOOTER_POWER);
-        } else {
-            leftShooter.setPower(0);
-            rightShooter.setPower(0);
-        }
-
-        dashboard.displayPrintf(1, LABEL_WIDTH, "digital input: ", "%b", inputPin);
-        dashboard.displayPrintf(2, LABEL_WIDTH, "servo position: ", "%1.3f", triggerServo.getPosition());
-        dashboard.displayPrintf(4, LABEL_WIDTH, "rotation: ", "%.2f", rotation);
-        */
-        dashboard.displayPrintf(1, LABEL_WIDTH, "speedLimiter: ", "%1.6f", armMotorSpeedLimiter);
-        dashboard.displayPrintf(2, LABEL_WIDTH, "imu z: ", "%.2f", gyro.getZRotationRate().value);
-        dashboard.displayPrintf(3, LABEL_WIDTH, "gamepad left stick mag: ", "%.2f", magnitude);
-        dashboard.displayPrintf(4, LABEL_WIDTH, "wristValue: ", "%1.2f", wristServo.getPosition());
-        dashboard.displayPrintf(5, LABEL_WIDTH, "leftPickupServo: ", "%1.2f", leftPickupServo.getPosition());
-        dashboard.displayPrintf(6, LABEL_WIDTH, "rightPickupServo: ", "%1.2f", rightPickupServo.getPosition());
-        dashboard.displayPrintf(7, LABEL_WIDTH, "gamepad left stick direction true: ", "%.2f", gamepad.getLeftStickDirectionDegrees(true));
-        dashboard.displayPrintf(8, LABEL_WIDTH, "gamepad left stick direction false: ", "%.2f", gamepad.getLeftStickDirectionDegrees(false));
-        dashboard.displayPrintf(9, LABEL_WIDTH, "gamepad right stick x: ", "%1.2f", gamepad.getRightStickX());
-        dashboard.displayPrintf(10, LABEL_WIDTH, "gamepad right stick y: ", "%1.2f", gamepad.getRightStickY());
-        dashboard.displayPrintf(11, LABEL_WIDTH, "y inverted: ", "%b", setYInverted);
-        dashboard.displayPrintf(12, LABEL_WIDTH, "x inverted: ", "%b", setXInverted);
-        dashboard.displayPrintf(13, LABEL_WIDTH, "fixedOnTarget: ", "%b", fixedOnTarget);
-        dashboard.displayPrintf(14, LABEL_WIDTH, "imu x: ", "%.2f", gyro.getRawXData(TrcGyro.DataType.HEADING).value);
-        dashboard.displayPrintf(15, LABEL_WIDTH, "imu y: ", "%.2f", gyro.getRawYData(TrcGyro.DataType.HEADING).value);
-        dashboard.displayPrintf(16, LABEL_WIDTH, "imu z: ", "%.2f", gyro.getRawZData(TrcGyro.DataType.HEADING).value);
-        dashboard.displayPrintf(17, LABEL_WIDTH, "gyro scale: ", "%.2f", gyroScale);
-        dashboard.displayPrintf(18, LABEL_WIDTH, "gyro kp: ", "%.2f", gyroKp);
-        jewelServo.setPosition(0.9);
-    }
-
-    private ElapsedTime getButtonElapsedTime(int button){
-        if(toggleTimeTracker.containsKey(button))
-            return toggleTimeTracker.get(button);
-        else {
-            toggleTimeTracker.put(button, new ElapsedTime());
-            return toggleTimeTracker.get(button);
-        }
-    }
-
-    @Override
-    public void gamepadButtonEvent(SWGamePad gamepad, int button, boolean pressed)
-    {
-        if (gamepad == this.gamepad)
-        {
-            switch (button)
-            {
-                case SWGamePad.GAMEPAD_A:
-                    //driveBase.enableGyroAssist(0.00001, 0.05);
-                    break;
-
-                case SWGamePad.GAMEPAD_Y:
-                    if(pressed && getButtonElapsedTime(button).seconds() > 1){
-                        fixedOnTarget = !fixedOnTarget;
-                        getButtonElapsedTime(button).reset();
-                    }
-                    break;
-
-                case SWGamePad.GAMEPAD_X:
-                    break;
-
-                case SWGamePad.GAMEPAD_B:
-                    //driveBase.disableGyroAssist();
-                    break;
-
-                case SWGamePad.GAMEPAD_RBUMPER:
-                    break;
-
-                /*
-                case SWGamePad.GAMEPAD_DPAD_DOWN:
-                    break;
-
-                case SWGamePad.GAMEPAD_DPAD_UP:
-                    if(pressed && getButtonElapsedTime(button).seconds() > 1){
-                        if(!checkToggleTime(button))
-                            break;
-
-                        setYInverted = !setYInverted;
-                        getButtonElapsedTime(button).reset();
-                    }
-                    break;
-                 */
-
-                case SWGamePad.GAMEPAD_DPAD_LEFT:
-                    break;
-
-                case SWGamePad.GAMEPAD_DPAD_RIGHT:
-                    if(pressed && getButtonElapsedTime(button).seconds() > 1){
-                        if(!checkToggleTime(button))
-                            break;
-
-                        setXInverted = !setXInverted;
-                        getButtonElapsedTime(button).reset();
-                    }
-                    break;
-            }
-        }
-    }
-
-    private boolean checkToggleTime(int button){
-        return (getButtonElapsedTime(button).seconds() > 0.75);
-    }
-
-    //THE THREAD THAT WILL RUN THE TRIGGER
-    @Override
-    public void run() {
-        /*
-        while (OP_MODE_IS_ACTIVE){
-            inputPin = digIn.getState();
-            if(!gamepad1.left_bumper) {
-                SHOOTER_MOTORS_REVERSE = false;
-                if (inputPin && gamepad1.right_bumper) {
-
-                    SHOOTER_MOTORS_ACTIVE = true;
-
-                    //if the shooter motors are not ready, SPEED THEM UP
-                    if (!SHOOTER_MOTORS_IS_READY) {
-                        for (long i = System.currentTimeMillis(); System.currentTimeMillis() - i < 250; ) {
-                            SHOOTER_MOTORS_ACTIVE = true;
-                        }
-
-                        //notify opmode that shooter motors are ready
-                        SHOOTER_MOTORS_IS_READY = true;
-                    }
-
-                    for (long i = System.currentTimeMillis(); System.currentTimeMillis() - i < 450; ) {
-                        triggerServo.setPosition(SHOOT_POSITION_TRIGGER);
-                    }
-                } else if (inputPin) {
-
-                    SHOOTER_MOTORS_ACTIVE = true;
-
-                    //if the shooter motors are not ready, SPEED THEM UP
-                    if (!SHOOTER_MOTORS_IS_READY) {
-                        for (long i = System.currentTimeMillis(); System.currentTimeMillis() - i < 250; ) {
-                            SHOOTER_MOTORS_ACTIVE = true;
-                        }
-                        //notify opmode that shooter motors are ready
-                        SHOOTER_MOTORS_IS_READY = true;
-                    }
-
-                    for (long i = System.currentTimeMillis(); System.currentTimeMillis() - i < 250; ) {
-                        triggerServo.setPosition(LOADED_POSITION_TRIGGER);
-                    }
-                } else {
-                    SHOOTER_MOTORS_ACTIVE = false;
-                    SHOOTER_MOTORS_IS_READY = false;
-
-                    for (long i = System.currentTimeMillis(); System.currentTimeMillis() - i < 250; ) {
-                        triggerServo.setPosition(LOADED_POSITION_TRIGGER);
-                    }
-                }
-            } else if (gamepad1.b) {
-                SHOOTER_MOTORS_ACTIVE = false;
-                SHOOTER_MOTORS_IS_READY = false;
-                SHOOTER_MOTORS_REVERSE = true;
-            }
-        }
-        */
+        telemetry.addData("magnitude", magnitude);
+        telemetry.addData("Arm Speed Limiter", armMotorSpeedLimiter);
+        telemetry.addData("turtle mode", turtleMode);
+        telemetry.update();
     }
 }
